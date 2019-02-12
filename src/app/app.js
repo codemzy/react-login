4e8fe338adcfa48285d79bd998363923910916fa
codemzy <codemzy@gmail.com>
2019-02-12 15:51:16 +0000

    Added login form component

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//components
+import LoginForm from './login/components/LoginForm';
+
+
 function App(props) {
   return (
     <div className="min-vh-100 bg-dark d-flex flex-column justify-content-center align-items-center">
-        <div className="spinner-border text-light" role="status">
-          <span className="sr-only">Loading...</span>
+      <div className="card shadow">
+        <div className="card-body">
+          <LoginForm />
         </div>
+      </div>
     </div>
   );
 }
