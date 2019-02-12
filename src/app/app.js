64356feec46304b42ab4766465a21866c022e5da
codemzy <codemzy@gmail.com>
2019-02-12 15:32:48 +0000

    Added loading spinner

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,14 @@
-console.log("testing again again");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+  return (
+    <div className="min-vh-100 bg-dark d-flex flex-column justify-content-center align-items-center">
+        <div className="spinner-border text-light" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
