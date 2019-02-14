dc55bf80f2a1b104ed8667cc19068a65573a3ef2
codemzy <codemzy@gmail.com>
2019-02-14 17:21:48 +0000

    Refactored register form

--- src/app/login/components/Register_Form.js
+++ src/app/login/components/Register_Form.js
@@ -8,36 +8,33 @@ class RegisterForm extends React.Component {
       password: '',
       confirm: ''
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  _handleChange(event) {
     const name = event.target.name;
     this.setState({[name]: event.target.value});
   }
 
-  handleSubmit(event) {
+  _handleSubmit(event) {
     alert('A form was submitted: ' + this.state.email);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this._handleSubmit.bind(this)}>
         <h6 className="text-muted">Sign up with your email address and password.</h6>
         <div className="form-group mt-4">
           <label>Email</label>
-          <input type="email" name="email" className="form-control" placeholder="you@youremail.com" value={this.state.email} onChange={this.handleChange} />
+          <input type="email" name="email" className="form-control" placeholder="you@youremail.com" value={this.state.email} onChange={this._handleChange.bind(this)} />
         </div>
         <div className="form-group">
           <label>Password</label>
-          <input type="password" name="password" className="form-control" value={this.state.password} onChange={this.handleChange} />
+          <input type="password" name="password" className="form-control" value={this.state.password} onChange={this._handleChange.bind(this)} />
         </div>
         <div className="form-group mb-4">
           <label>Confirm Password</label>
-          <input type="password" name="confirm" className="form-control" value={this.state.confirm} onChange={this.handleChange} />
+          <input type="password" name="confirm" className="form-control" value={this.state.confirm} onChange={this._handleChange.bind(this)} />
         </div>
         <button type="submit" className="btn btn-primary btn-block">Create Account</button>
       </form>
