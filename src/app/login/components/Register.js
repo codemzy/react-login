fd75c67be72278c35c4fc3a537b5c6797bee6a74
codemzy <codemzy@gmail.com>
2019-02-14 17:27:18 +0000

    Updated document title on register component

--- src/app/login/components/Register.js
+++ src/app/login/components/Register.js
@@ -7,6 +7,7 @@ import Main from './Main';
 import RegisterForm from './Register_Form';
 
 function Register(props) {
+  document.title = "Register"; // move this to a shared component with app name
   return (
     <Main title="Register" footer={<small><p className="text-white">Already have an account? <Link to={basePath + "/login"} className="text-white"><u>Log In</u></Link></p></small>}>
       <RegisterForm />
