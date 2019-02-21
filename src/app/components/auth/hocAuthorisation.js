38f9d0f57b3faf902d8e48536f6ec01c48ddc6da
codemzy <codemzy@gmail.com>
2019-02-21 17:38:08 +0000

    Added appPath to authorisation redirects

--- src/app/components/auth/hocAuthorisation.js
+++ src/app/components/auth/hocAuthorisation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { appPath } from '../../config/settings';
 
 // context for user state
 import { UserContext } from '../../config/context';
@@ -11,9 +12,9 @@ const withAuthorisation = (ComposedComponent, authRequired, roleRequired) => {
       <UserContext.Consumer>
         {function(user) {
           if (authRequired && !user) { // must be logged in to view
-            return <Redirect to="/login" />;
+            return <Redirect to={appPath + "/login"} />;
           } else if (!authRequired && user) { // must be logged out to view
-            return <Redirect to="/" />;
+            return <Redirect to={appPath + "/"} />;
           } else { // authorisation granted
             return <ComposedComponent {...props} />;
           }
