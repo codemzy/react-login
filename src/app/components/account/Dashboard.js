81a63f9742b5bb7eecafc0a8b1c0109dbe1f3730
codemzy <codemzy@gmail.com>
2019-03-01 16:33:52 +0000

    Added Dashboard page

--- src/app/components/account/Dashboard.js
+++ src/app/components/account/Dashboard.js
@@ -10,7 +10,8 @@ function Dashboard(props) {
   return (
     <Main>
       <div className="text-center">
-        <h1 className="display-1">Dashboard</h1>
+        <h1 className="display-3">Dashboard</h1>
+        <p className="lead">This is a dashboard that only a logged in user can see.</p>
       </div>
     </Main>
   );
