2e4dcb94f645c7724fece0c78c2222d2828cfd87
codemzy <codemzy@gmail.com>
2019-03-01 14:48:08 +0000

    Added dummy user object

--- src/app/api/user.js
+++ src/app/api/user.js
@@ -1,7 +1,8 @@
 // fake check auth - wire this up to a real api
   // for now this just fakes an api request with a timeout
 export const checkAuth = new Promise(function(resolve, reject) {
+  let dummyUser = { firstName: "Fake", lastName: "User" };
   setTimeout(function() {
-    resolve(true); // true for logged in or false for not
+    resolve(dummyUser); // return dummyUser for logged in, or false for not logged in
   }, 5000);
 });
