c950298b65cd730b383c39ecb7e9d01cf32bdf70
codemzy <codemzy@gmail.com>
2019-03-01 15:38:20 +0000

    Added Nav component

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// components
+import Nav from './Nav';
+
 function Main(props) {
   return (
     <div className="min-vh-100 d-flex flex-column">
+      <Nav />
       {props.children}
     </div>
   );
