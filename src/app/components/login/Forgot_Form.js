6ea37e3dc6fb6347040f61ddd2ec1fc8cf52c915
codemzy <codemzy@gmail.com>
2019-03-06 16:30:07 +0000

    Added validation to forgot password

--- src/app/components/login/Forgot_Form.js
+++ src/app/components/login/Forgot_Form.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+// validate
+import { checkEmail } from '../../utils/validate';
+
 class ForgotForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ''
+      email: '',
+      loading: false,
+      errors: {}
     };
   }
 
@@ -14,8 +19,10 @@ class ForgotForm extends React.Component {
   }
 
   _handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.email);
     event.preventDefault();
+    // validate data
+    const ERRORS = { email: checkEmail(this.state.email) };
+    this.setState({ errors: ERRORS });
   }
 
   render() {
@@ -24,7 +31,8 @@ class ForgotForm extends React.Component {
         <h6 className="text-muted">Please submit your email address. You'll get sent a link to reset your password.</h6>
         <div className="form-group my-4">
           <label>Email</label>
-          <input type="email" name="email" className="form-control" placeholder="you@youremail.com" value={this.state.email} onChange={this._handleChange.bind(this)} />
+          <input type="email" name="email" className={"form-control" + (this.state.errors.email ? " is-invalid" : "")} placeholder="you@youremail.com" value={this.state.email} onChange={this._handleChange.bind(this)} />
+          { this.state.errors.email ? <small className="invalid-feedback">{this.state.errors.email}</small> : false }
         </div>
         <button type="submit" className="btn btn-primary btn-block">Get Reset Email</button>
       </form>
