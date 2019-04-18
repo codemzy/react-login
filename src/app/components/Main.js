39b7c3071e352ac3d912056aae305ea69df96f15
Codemzy <codemzy@gmail.com>
2019-04-18 14:21:56 +0000

    Added alert component and wired up

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -3,12 +3,18 @@ import React from 'react';
 // components
 import Nav from './Nav';
 import Footer from './Footer';
+import Alert from './Alert';
 
 function Main(props) {
   return (
     <div className="min-vh-100 d-flex flex-column">
       <Nav />
-      <div className="flex-grow-1">{props.children}</div>
+      <div className="flex-grow-1">
+        <div className="container">
+          <Alert />
+          {props.children}
+        </div>
+      </div>
       <Footer />
     </div>
   );
